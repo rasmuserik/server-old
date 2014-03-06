b0ab4be9374f0fcad2111c9033b2bbfb7005a8dc
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-03-06 12:24:19 +0100

    server-running script

--- keeprunning.sh
+++ keeprunning.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+export PATH=/home/server/local/bin:$PATH
 while true; do
   if nc -z localhost 8888; then
     exit 0
   fi
+  git checkout .
+  git pull
   npm install
-  API_PORT=8888 xinit `which npm` start -- :8
+  API_PORT=8888 xinit `which npm` start -- :8 >> keeprunning.log 2>&1
 done
